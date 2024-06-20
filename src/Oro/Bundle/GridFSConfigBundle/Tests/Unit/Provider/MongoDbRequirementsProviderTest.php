e78e08712f10c2dd496a82f2d69e1fe172da1de6
Oleksandr Protsiuk <44535554+OleksandrProtsiuk@users.noreply.github.com>
2024-06-20 15:49:54 +0300

    BAP-22655: Do not require a writable public/media folder (#38747)
    - public/media requirement check moved to PublicMediaDirectoryRequirementsProvider
    - Added MongoDB requirements provider
    - Added config to override PublicMediaDirectoryRequirementsProvider with MongoDb provider when GridFSConfigBundle is used

---
+++ src/Oro/Bundle/GridFSConfigBundle/Tests/Unit/Provider/MongoDbRequirementsProviderTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Oro\Bundle\GridFSConfigBundle\Tests\Unit\Provider;
+
+use Oro\Bundle\GridFSConfigBundle\Provider\MongoDbDriverConfig;
+use Oro\Bundle\GridFSConfigBundle\Provider\MongoDbRequirementsProvider;
+use Psr\Log\LoggerInterface;
+use Symfony\Requirements\RequirementCollection;
+
+class MongoDbRequirementsProviderTest extends \PHPUnit\Framework\TestCase
+{
+    private string $mediaDirectory;
+    private MongoDbRequirementsProvider $provider;
+
+    protected function setUp(): void
+    {
+        $projectDirectory = sys_get_temp_dir();
+        $this->mediaDirectory = '/public/media';
+
+        $this->mongoDbDriverConfig = $this->createMock(MongoDbDriverConfig::class);
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $this->provider = new MongoDbRequirementsProvider(
+            $projectDirectory,
+            $this->mongoDbDriverConfig,
+            $logger
+        );
+
+        $this->tempDir = sys_get_temp_dir() . $this->mediaDirectory;
+        mkdir($this->tempDir, 0777, true);
+    }
+
+    protected function tearDown(): void
+    {
+        chmod($this->tempDir, 0777);
+        rmdir($this->tempDir);
+        rmdir(sys_get_temp_dir() . '/public');
+    }
+
+    public function testGetOroRequirementsPositive(): void
+    {
+        $this->mongoDbDriverConfig->expects($this->once())
+            ->method('isConnected')
+            ->willReturn(false);
+
+        $collection = $this->provider->getOroRequirements();
+
+        $this->assertInstanceOf(RequirementCollection::class, $collection);
+
+        $requirements = $collection->all();
+        $this->assertNotEmpty($requirements);
+        $this->assertStringContainsString(
+            'public/media directory must be writable',
+            $requirements[0]->getTestMessage()
+        );
+    }
+
+    public function testGetOroRequirementsNegative(): void
+    {
+        $this->mongoDbDriverConfig->expects($this->once())
+            ->method('isConnected')
+            ->willReturn(true);
+
+        $collection = $this->provider->getOroRequirements();
+
+        $this->assertNull($collection);
+    }
+
+    public function testAddPathWritableRequirementIsFulfilled(): void
+    {
+        $reflection = new \ReflectionClass($this->provider);
+        $method = $reflection->getMethod('addPathWritableRequirement');
+
+        $collection = $method->invoke($this->provider, $this->mediaDirectory);
+
+        $requirements = $collection->all();
+        $this->assertNotEmpty($requirements);
+        $this->assertTrue($requirements[0]->isFulfilled());
+    }
+
+    public function testAddPathWritableRequirementIsNotFulfilled(): void
+    {
+        // Change permissions to Read-Only
+        chmod($this->tempDir, 0555);
+
+        $reflection = new \ReflectionClass($this->provider);
+        $method = $reflection->getMethod('addPathWritableRequirement');
+
+        $collection = $method->invoke($this->provider, $this->mediaDirectory);
+
+        $requirements = $collection->all();
+        $this->assertNotEmpty($requirements);
+        $this->assertFalse($requirements[0]->isFulfilled());
+    }
+}
