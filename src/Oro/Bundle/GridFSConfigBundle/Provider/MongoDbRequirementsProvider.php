258a0deb3811c1739969941ff824ff17d7ac823d
Oleksandr Protsiuk <44535554+OleksandrProtsiuk@users.noreply.github.com>
2024-07-16 16:16:51 +0300

    BAP-22655: Do not require a writable public/media folder (#38764)
    - public/media requirement check moved to PublicMediaDirectoryRequirementsProvider
    - Added default MongoDB driver config to check connection status
    - Added MongoDB requirements provider
    - Added config to override PublicMediaDirectoryRequirementsProvider with MongoDb provider when GridFSConfigBundle is used

---
+++ src/Oro/Bundle/GridFSConfigBundle/Provider/MongoDbRequirementsProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oro\Bundle\GridFSConfigBundle\Provider;
+
+use Oro\Bundle\InstallerBundle\Provider\AbstractRequirementsProvider;
+use Psr\Log\LoggerInterface;
+use Symfony\Requirements\RequirementCollection;
+
+/**
+ * MongoDB requirements provider
+ */
+class MongoDbRequirementsProvider extends AbstractRequirementsProvider
+{
+    public function __construct(
+        protected string $projectDirectory,
+        private ?MongoDbDriverConfig $mongoDbDriverConfig,
+        private LoggerInterface $logger,
+    ) {
+    }
+
+    /**
+     * @inhericDoc
+     */
+    public function getOroRequirements(): ?RequirementCollection
+    {
+        if (!$this->mongoDbDriverConfig) {
+            return $this->addPathWritableRequirement('public/media');
+        }
+
+        if (!$this->mongoDbDriverConfig->isConnected($this->logger)) {
+            return $this->addPathWritableRequirement('public/media');
+        }
+
+        return null;
+    }
+
+    protected function addPathWritableRequirement(string $path): RequirementCollection
+    {
+        $collection = new RequirementCollection();
+
+        $fullPath = $this->projectDirectory . '/' . $path;
+        $pathType = is_file($fullPath) ? 'file' : 'directory';
+
+        $collection->addRequirement(
+            is_writable($fullPath),
+            $path . ' directory must be writable',
+            'Change the permissions of the "<strong>' . $path . '</strong>" ' . $pathType . ' so' .
+            ' that the web server can write into it.'
+        );
+
+        return $collection;
+    }
+}
